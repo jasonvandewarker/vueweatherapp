460e187bf7f5ff90a41b890901057164b2511f06
Jason Vandewarker <jason@sightly.com>
2017-10-20 11:56:48 -0700

    Not working correctly.

--- src/components/GetQuote.vue
+++ src/components/GetQuote.vue
@@ -1,21 +1,35 @@
 <template>
     <div>
+        <div class="container">
 <h1>Testing out this Vue thing.</h1>  
 
 <div class="well">
-    <p>Here's some stuff</p>
-    <button type="button" class="btn btn-primary">Push this button</button>
-
+    <p>Here's some stuff.</p>
+    <button type="button" class="btn btn-primary" v-on:click="getQuote()">Push this button</button>
+    <p>{{ quote }}</p>
 </div>
-
-
-
-
+</div>
 </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      quote: ''
+    }
+  },
+  methods: {
+    qetQuote () {
+      console.log('Stuff')
+      this.$http
+        .get('https://api.chucknorris.io/jokes/random', (data) => {
+          this.quote = data
+        })
+        .error((err) => console.log(err))
+    }
+  }
+
 }
 </script>
 
